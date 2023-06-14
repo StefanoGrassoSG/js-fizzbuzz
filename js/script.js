a8f35f0f736d79f8a8cde2161e2466b385e9e164
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-14 17:15:35 +0200

    listener on all buttons

--- js/script.js
+++ js/script.js
@@ -7,31 +7,38 @@ const ul = document.querySelector('#numlist');
 for (i; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
-        const li = document.createElement('div');
+        const li = document.createElement('button');
         li.innerHTML = 'FizzBuzz';
         ul.append(li);
         li.className += 'bgblue';
     }
 
     else if (i % 3 == 0) {
-        const li = document.createElement('div');
+        const li = document.createElement('button');
         li.innerHTML = 'Fizz';
         ul.append(li);
         li.className += 'bgorange';
     }
 
     else if (i % 5 == 0) {
-        const li = document.createElement('div');
+        const li = document.createElement('button');
         li.innerHTML = 'Buzz';
         ul.append(li);
         li.className += 'bgyellow';
     }
     
     else {
-        const li = document.createElement('div');
+        const li = document.createElement('button');
         li.innerHTML = i;
         ul.append(li);
         li.className += 'bglightblue';
     }
 };
 
+let buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        alert('bravo!')
+    });
+}

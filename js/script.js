a89e22cfc8fb01d1ba51da687d4896b63767837c
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-14 16:13:18 +0200

    finish style

--- js/script.js
+++ js/script.js
@@ -7,27 +7,31 @@ const ul = document.querySelector('#numlist');
 for (i; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
-        const li = document.createElement('li');
+        const li = document.createElement('div');
         li.innerHTML = 'FizzBuzz';
         ul.append(li);
+        li.className += 'bgblue';
     }
 
     else if (i % 3 == 0) {
-        const li = document.createElement('li');
+        const li = document.createElement('div');
         li.innerHTML = 'Fizz';
         ul.append(li);
+        li.className += 'bgorange';
     }
 
     else if (i % 5 == 0) {
-        const li = document.createElement('li');
+        const li = document.createElement('div');
         li.innerHTML = 'Buzz';
         ul.append(li);
+        li.className += 'bgyellow';
     }
     
     else {
-        const li = document.createElement('li');
+        const li = document.createElement('div');
         li.innerHTML = i;
         ul.append(li);
+        li.className += 'bglightblue';
     }
 };
 
